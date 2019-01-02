1f25bc221a7ea24ee3f9ebbbf50112c4b9aa888e
imorandinwnp <imorandin@wnpower.com>
2019-01-02 16:31:25 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # NodeJS-Install
-Instalador de NodeJS en Linux
+Instalador de NodeJS en Linux y cPanel basado en el artículo https://help.wnpower.com/hc/es/articles/360018540771-Como-instalar-Node-js
+
+## Modo de uso:
+### Linux
+
+    wget https://raw.githubusercontent.com/wnpower/NodeJS-Install && bash linux_install_nodejs.sh
+
---
+++ linux_install_nodejs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+ART_KB="https://help.wnpower.com/hc/es/articles/360018540771-Como-instalar-Node-js"
+
+NODE_VERSIONS=$(curl -s https://nodejs.org/dist/ | sed 's/<[^>]\+>//g' | grep "latest.*/" | awk '{print $1}' | sed 's/latest-//g' | sed 's/latest//g' | sed 's/\///g' | sed ':a;N;$!ba;s/\n/ /g')
+
+if (echo "$NODE_VERSIONS" | grep "v.*/" > /dev/null); then
+	echo "No se puede conectar a https://nodejs.org/dist/, probá la instalación manual: $ART_KB"
+fi
+
+select VERSION in $NODE_VERSIONS
+do
+	FILE=$(curl -s https://nodejs.org/dist/latest-$VERSION/ | sed 's/<[^>]\+>//g' | grep "linux-x64.tar.gz" | awk '{print $1}')
+	BIN=$(echo $FILE | sed 's/.tar.gz$//')
+	echo "Se bajará $FILE..."
+
+	cd ~
+	
+	echo "Descargando $FILE..."
+	wget $FILE -O ./$FILE
+
+	echo "Descomprimiendo $FILE..."
+	tar xvfz $FILE
+	mv $BIN nodejs
+	rm -f $FILE
+
+	mkdir ~/bin
+	cp nodejs/bin/node ~/bin
+	cd ~/bin
+	ln -s ../nodejs/lib/node_modules/npm/bin/npm-cli.js npm
+
+	echo "Agregando PATH..."
+
+	sed -i '/PATH=\$PATH:\$HOME\/bin/d' ~/.bashrc
+	sed -i '/export PATH/d' ~/.bashrc
+ 
+	echo 'PATH=$PATH:$HOME/bin' >> ~/.bashrc
+	echo 'export PATH' >> ~/.bashrc
+ 
+	PATH=$PATH:$HOME/bin
+	export PATH
+
+	echo "Comprobando instalación..."
+
+	node --version
+	npm --version
+
+	echo "¡Listo!. Para más info y código de ejemplo ingresá a $ART_KB"
+
+	exit 1
+done
